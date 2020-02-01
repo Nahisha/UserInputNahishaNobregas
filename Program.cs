ddb307eff17e9c6a1831aeb489bf59ce2fa2ef8e
Nahisha <Nahisha@DESKTOP-D87FS9K>
2020-01-31 21:08:41 -0500

    Added if else if conditions.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace UserInputNahishaNobregas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hey, how are you? This is Nahisha Nobregas!");
+            string greeting = "Hey, how are you? This is Nahisha Nobregas!";
+            string nextGreeting = "";
+
+            if (greeting == "Hey, how are you? This is Nahisha Nobregas!")
+            {
+                nextGreeting = "I'm doing great, thank you!";
+            }
+            else if (greeting == "Hey there!")
+            {
+                nextGreeting = "How are you?"; 
+            }
+            else if (greeting == "Are you doing well?")
+            {
+                nextGreeting = "Yes I am, thank you for asking!";
+            }
+
+            Console.WriteLine(nextGreeting);
+            
+     
+           
         }
     }
 }
